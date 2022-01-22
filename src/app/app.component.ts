ecf040024f6f193c219f91954ba53892fb5ec9ca
masixolematolweni.1@gmail.com <72172920+Matolweni95@users.noreply.github.com>
2022-01-22 22:54:43 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'task-adder';
+  test:any;
+  list:any[] = [];
+
+  myForm!: FormGroup;
+
+  constructor (private fb: FormBuilder){
+    this.myForm = this.fb.group({
+      name: [''], 
+      
+      });
+  }
+  
+
+  res(){
+    let type = (<HTMLInputElement>document.getElementById('a')).value;
+    this.test = type;
+    this.list.push(this.test);
+    this.myForm.reset();
+ 
+
+  }
 }
